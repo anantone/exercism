f08e0c169488998967bc9e9ddc94d0483e790c32
anantone <anantone@anant1.net>
2025-07-22 06:57:29 +0200

    patched manually

--- ruby/dnd-character/dnd_character.rb
+++ ruby/dnd-character/dnd_character.rb
@@ -12,20 +12,33 @@ class DndCharacter
 
   end
 
+  def self.modifier(constitution)
+      (constitution - 10)/2
+    end
+
+  private
+
+  attr_writer :strength,
+              :dexterity,
+              :constitution,
+              :intelligence,
+              :wisdom,
+              :charisma,
+              :hitpoints
+
   def initialize
-    self.strength      = Die.roll.sum
-    self.dexterity     = Die.roll.sum
-    self.constitution  = Die.roll.sum
-    self.intelligence  = Die.roll.sum
-    self.wisdom        = Die.roll.sum
-    self.charisma      = Die.roll.sum
+    self.strength,
+    self.dexterity,
+    self.constitution,
+    self.intelligence,
+    self.wisdom,
+    self.charisma      = 6.times.map { Die.roll.sum }
     self.hitpoints     = BASE_HITPOINTS + self.class.modifier(constitution)
   end
 
-  def self.modifier(constitution)
-    (constitution - 10)/2
-  end
-  
+
+  public
+
   attr_accessor :strength,
                 :dexterity,
                 :constitution,
@@ -33,5 +46,5 @@ class DndCharacter
                 :wisdom,
                 :charisma,
                 :hitpoints
-  
+
 end
